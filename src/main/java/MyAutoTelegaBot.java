bd45979e7d1c4214a266b6d0f8a45187601d08fa
Gigalskiy <gigalskiy@gmail.com>
2021-11-11 17:39:46 +0300

    login and key

--- src/main/java/MyAutoTelegaBot.java
+++ src/main/java/MyAutoTelegaBot.java
@@ -5,6 +5,8 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.validation.OverridesAttribute;
 import java.util.*;
 
 
@@ -23,38 +25,56 @@ public class MyAutoTelegaBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        /* ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setOneTimeKeyboard(false);
+        keyboardMarkup.setResizeKeyboard(true);
 
 
-        if (update.hasMessage() && update.getMessage().hasText()) {
+            KeyboardButton k1 = new KeyboardButton("Привет");
+            KeyboardButton k2 = new KeyboardButton("работает");
+            KeyboardButton k3 = new KeyboardButton("когда");
+            ArrayList<KeyboardRow> kbr = new ArrayList<>();
+            KeyboardRow kbr1 = new KeyboardRow();
+            KeyboardRow kbr2 = new KeyboardRow();
+            KeyboardRow kbr3 = new KeyboardRow();
+            kbr1.add(k1) ;
+            kbr2.add(k2);
+            kbr3.add(k3);
+            kbr.add(kbr1);
+            kbr.add(kbr2);
+            kbr.add(kbr3);
+
+            keyboardMarkup.setKeyboard(kbr);
+             if (update.hasMessage() && !update.getMessage().hasText()) {
             SendMessage message = new SendMessage(); // Создайте объект SendMessage с обязательными полями
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(logicBots.BotResponse(update.getMessage().getText()));
-            setKeybord(message);
+            // message.setChatId(update.getMessage().getChatId().toString());
+            //message.setText(logicBots.BotResponse(update.getMessage().getText()));
+            message.setReplyMarkup(keyboardMarkup);
             try {
                 execute(message); // Способ вызова для отправки сообщения
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
+            kbr.clear();
+             } */
+
+        if (update.hasMessage() && update.getMessage().hasText() ) {
+            SendMessage message = new SendMessage(); // Создайте объект SendMessage с обязательными полями
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(logicBots.BotResponse(update.getMessage().getText()));
+
+            try {
+              execute(message); // Способ вызова для отправки сообщения
+            } catch (TelegramApiException e) {
+              e.printStackTrace();
+            }
+
+             //KeyboardButton kb1 = new KeyboardButton("Вопрос");
+             //KeyboardButton kb2 = new KeyboardButton("Ответ");
+
+
         }
     }
 
-    public void setKeybord(SendMessage sendMessage) {
-         ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-         keyboardMarkup.setOneTimeKeyboard(false);
-         keyboardMarkup.setResizeKeyboard(true);
-         keyboardMarkup.setSelective(true);
-         ArrayList<KeyboardRow> keybord = new ArrayList<>();
-         KeyboardRow kbr1 = new KeyboardRow();
-         KeyboardRow kbr2 = new KeyboardRow();
-         KeyboardRow kbr3 = new KeyboardRow();
-         kbr1.add(new KeyboardButton("Привет")) ;
-         kbr2.add(new KeyboardButton("Работаете"));
-         kbr3.add(new KeyboardButton("Когда"));
-         keybord.add(kbr1);
-         keybord.add(kbr2);
-         keybord.add(kbr3);
-         keyboardMarkup.setKeyboard(keybord);
-         sendMessage.setReplyMarkup(keyboardMarkup);
-    }
 
 }
